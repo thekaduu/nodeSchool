e82fdd9add65e531d61b85283a8954c253597de8
Carlos Edurdo Lima Braz <carloseduardo@lanwise.com.br>
2015-09-01 01:21:43 -0300

    Iniciando Express + ElasticSearch

---
+++ eSchool/public/javascripts/Apps/home_controller.js
@@ -0,0 +1,16 @@
+var app = angular.module('eSchool', ['ngResource']);
+app.controller('HomeController',function($scope,$resource){
+	client.ping({
+  // ping usually has a 3000ms timeout
+  requestTimeout: Infinity,
+
+  // undocumented params are appended to the query string
+  hello: "elasticsearch!"
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+})
